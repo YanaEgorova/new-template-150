0338e9f9e66273fdf7bb8945dfebc4e186e45d72
Yana <yanaegorova69.3@gmail.com>
2022-09-28 23:47:19 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -19,19 +19,10 @@ export const item = (product) => {
       $${(product.price).toFixed(2)}
       </span>
       <div class="product__btn-block">
-      <a href="product-page.html?id=${product.id}" class="policy__btn btn--first"><span>details</span>             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-      viewBox="0 0 476.213 476.213" style="enable-background:new 0 0 476.213 476.213;" xml:space="preserve">
-   <polygon points="345.606,107.5 324.394,128.713 418.787,223.107 0,223.107 0,253.107 418.787,253.107 324.394,347.5 
-     345.606,368.713 476.213,238.106 "/>
-   </svg>
-  </a>
-
-  <button class="policy__btn js_add-to-cart"><span>buy</span>             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-  viewBox="0 0 476.213 476.213" style="enable-background:new 0 0 476.213 476.213;" xml:space="preserve">
-<polygon points="345.606,107.5 324.394,128.713 418.787,223.107 0,223.107 0,253.107 418.787,253.107 324.394,347.5 
- 345.606,368.713 476.213,238.106 "/>
-</svg>
-</button>
+      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+      ${
+        product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+      }
     </div>
       </div>
 
@@ -42,5 +33,6 @@ export const item = (product) => {
   </li>
     `
 }
+
 
 
